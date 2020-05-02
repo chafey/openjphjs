83e797759e141aa93c926a5601a37af9830dd1b9
Chris Hafey <chafey@gmail.com>
2020-05-02 22:00:34 +0000

    updated openjph header file includes

--- src/HTJ2KDecoder.hpp
+++ src/HTJ2KDecoder.hpp
@@ -6,11 +6,11 @@
 #include <exception>
 #include <memory>
 
-#include "../extern/OpenJPH/src/core/common/ojph_arch.h"
-#include "../extern/OpenJPH/src/core/common/ojph_file.h"
-#include "../extern/OpenJPH/src/core/common/ojph_mem.h"
-#include "../extern/OpenJPH/src/core/common/ojph_params.h"
-#include "../extern/OpenJPH/src/core/common/ojph_codestream.h"
+#include <ojph_arch.h>
+#include <ojph_file.h>
+#include <ojph_mem.h>
+#include <ojph_params.h>
+#include <ojph_codestream.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten/val.h>
