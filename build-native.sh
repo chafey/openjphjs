be9a2fcee4793b9eec346f72477675d9a37aba29
Chris Hafey <chafey@gmail.com>
2020-08-29 03:40:22 +0000

    MAINT: Misc updates 1) Switched to official emscripten docker image 2) Pegged emscripten to version 1.39.5 which is known to work 3) Updated to latest OpenJPH version 4) build-native.sh now builds to build-native folder 5) Generated new WASM builds

--- build-native.sh
+++ build-native.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir -p build
-(cd build && cmake -DCMAKE_BUILD_TYPE=Debug ..)
+mkdir -p build-native
+(cd build-native && cmake -DCMAKE_BUILD_TYPE=Debug ..)
 #(cd build && cmake ..)
-(cd build && make VERBOSE=1 -j 8)
-(build/test/cpp/cpptest)
+(cd build-native && make VERBOSE=1 -j 8)
+(build-native/test/cpp/cpptest)
